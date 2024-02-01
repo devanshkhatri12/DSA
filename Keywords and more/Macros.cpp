90a7b30135dfc6393f110fc3893f53dd94a0a09a
devanshkhatri12 <dkkhatri941@gmail.com>
2024-02-01 17:06:26 +0530

    Macros in  C++

--- Keywords and more/Macros.cpp
+++ Keywords and more/Macros.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-#define PI 3.14;
+#define PI 3.14
 
 int main() 
 {
-    int r=5;
+    int radius =5;
 
-    double area = PI + r;
+    double area = PI * radius * radius;
 
-    cout<<"Area "<<area<<endl;
+    cout<<"Area of circle "<<area<<endl;
 }
