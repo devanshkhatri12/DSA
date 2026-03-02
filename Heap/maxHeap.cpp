cbfbfdc3141543a09d4e5e7502a7f8378e51d7a1
devanshkhatri12 <dkkhatri941@gmail.com>
2026-03-03 00:07:20 +0530

    heapify algo using max heap

--- Heap/maxHeap.cpp
+++ Heap/maxHeap.cpp
@@ -66,8 +66,27 @@ class heap{
         }
     }
 
-
 };
+
+    void heapify(vector<int>& arr, int size, int i)
+    {
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i + 1;
+
+        if(left < size && arr[largest] < arr[left]){
+            largest = left;
+        }
+
+        if(right < size && arr[largest] < arr[right]){
+            largest = right;
+        }
+
+        if(largest != i){
+            swap(arr[largest] , arr[i]);
+            heapify(arr, size, largest);
+        }
+    }
 
 int main()
 {
@@ -81,6 +100,19 @@ int main()
 
     h.deleteNode();
     h.print();
+
+    vector<int> arr = {-1, 54, 53, 55, 52, 50};
+    int size = 5;
+
+    for(int i=size/2; i>0; i--){
+        heapify(arr, size, i);
+    }
+
+    cout<<"print heap"<<endl;
+    for(int i=1; i<=size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }cout<<endl;
 
     return 0;
 }
